cd61d59d820a07165d18da0a24d3a01ab406fb18
EI.KMiller <KMiller.eilexinteractive@gmail.com>
2022-09-06 20:13:05 +1000

    Moved spawn code to the scene controller so all scenes can use it

--- Scripts/Rooms/Room_2.cs
+++ Scripts/Rooms/Room_2.cs
@@ -1,31 +1,9 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 public class Room_2 : SceneController
 {
-    [Export] private PackedScene EnemyPrefab;
-    private List<Vector2> _SpawnPoints = new List<Vector2>();
-    private int _NumOfEnemies = 0;
-
-    public override void _Ready()
-    {
-        base._Ready();
-
-        _NumOfEnemies = (int)GD.RandRange(4, 10);
-        var spawnPoints = GetTree().GetNodesInGroup("SpawnPoint");
-        for (int i = 0; i < _NumOfEnemies; ++i)
-        {
-            var enemyInstance = EnemyPrefab.Instance();
-            AddChild(enemyInstance);
-
-            var enemyController = enemyInstance as EnemyController;
-            if (enemyController != null)
-            {
-                var spawnNode = spawnPoints[(int)GD.RandRange(0, spawnPoints.Count - 1)] as Node2D;
-                enemyController.Position = spawnNode.GlobalPosition;
-                enemyController.CanMove = true;
-            }
-        }
-    }
+    
 }
--- Scripts/SceneController.cs
+++ Scripts/SceneController.cs
@@ -8,6 +8,52 @@ public class SceneController : Node2D
 
     [Export] protected int _MinEnemySpawn;                // Minimum amount of enemies that will spawn
     [Export] protected int _MaxEnemySpawn;                // Maximum amount of enemies that will spawn
+    protected int _NumOfEnemies;
+    
+    
+    [Export] protected PackedScene EnemyPrefab;
+    
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        var spawnPoints = GetTree().GetNodesInGroup("SpawnPoint");
+        _NumOfEnemies = (int)GD.RandRange(_MinEnemySpawn, _MaxEnemySpawn);
+        for (int i = 0; i < _NumOfEnemies; ++i)
+        {
+            int loopCount = 0;
+            while (true)
+            {
+                // Prevent infinity loop
+                if (loopCount >= 20)
+                    break;
+                
+                // Get a random spawn point
+                var point = spawnPoints[(int)GD.RandRange(0, spawnPoints.Count - 1)];
+                var pointController = point as SpawnPointController;
+                // Check the point is valid and not been used already
+                if (pointController != null && !pointController.HasBeenUsed)
+                {
+                    // Create an instance of the enemy and load spawn it
+                    var instance = EnemyPrefab.Instance();
+                    AddChild(instance);
+                    
+                    // Get the instance as the enemy controller & set the position to the spawn point
+                    var enemy = instance as EnemyController;
+                    if (enemy != null)
+                        enemy.Position = pointController.Position;
+                    
+                    
+                    loopCount = 0;              // Reset the loop count
+                    break;                      // Leave the while loop
+                }
+                else
+                {
+                    loopCount++;
+                }
+            }
+        }
+    }
     
     public virtual void SaveRoom()
     {
