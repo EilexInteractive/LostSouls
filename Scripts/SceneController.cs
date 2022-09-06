9701007eef1e0038360bbc6bdcee860c7cba2aa5
EI.KMiller <KMiller.eilexinteractive@gmail.com>
2022-09-06 19:35:22 +1000

    Added variables to determine how many enemies will be in a room

--- Scripts/SceneController.cs
+++ Scripts/SceneController.cs
@@ -4,7 +4,10 @@ using System;
 public class SceneController : Node2D
 {
     [Export] protected int _RoomLevel;                    // What level of difficulty this level is
-    protected RoomData _SaveData;
+    protected RoomData _SaveData;                           // Reference to the save data of the room
+
+    [Export] protected int _MinEnemySpawn;                // Minimum amount of enemies that will spawn
+    [Export] protected int _MaxEnemySpawn;                // Maximum amount of enemies that will spawn
     
     public virtual void SaveRoom()
     {
