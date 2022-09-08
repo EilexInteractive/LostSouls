3735d300e35fb46f1d0f0d30676f482495fd5456
EI.KMiller <KMiller.eilexinteractive@gmail.com>
2022-09-08 11:20:13 +1000

    Moved folder and added room name

--- Scripts/Rooms/SceneController.cs
+++ Scripts/Rooms/SceneController.cs
@@ -5,6 +5,7 @@ public class SceneController : Node2D
 {
     [Export] protected int _RoomLevel;                    // What level of difficulty this level is
     protected RoomData _SaveData;                           // Reference to the save data of the room
+    [Export] protected string _RoomName;
 
     [Export] protected int _MinEnemySpawn;                // Minimum amount of enemies that will spawn
     [Export] protected int _MaxEnemySpawn;                // Maximum amount of enemies that will spawn
@@ -75,7 +76,7 @@ public class SceneController : Node2D
     public virtual void LoadRoomData()
     {
         GameController gc = GetNode<GameController>("/root/GameController");
-        _SaveData = gc?.LoadRoomData("Main");
+        _SaveData = gc?.LoadRoomData(_RoomName);                // Load this room
     }
 
     public int GetRoomLevel() => _RoomLevel;
