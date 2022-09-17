74ea46e74f585609ee4a1f7ed131fd16bb9cc180
EI.KMiller <KMiller.eilexinteractive@gmail.com>
2022-09-18 09:17:43 +1000

    Created the settings controller class

--- Scripts/SettingsController.cs
+++ Scripts/SettingsController.cs
@@ -13,4 +13,33 @@ public class SettingsController : Control
         _MusicSlider = GetNode<HSlider>("MusicSlider");
         _SFXSlider = GetNode<HSlider>("SFXSlider");
     }
+
+    public void SaveSettings()
+    {
+        Settings newSettings = new Settings();
+
+        // Get the audio values
+        if(_MusicSlider != null && _SFXSlider != null)
+        {
+            newSettings._MusicLevel = (float)_MusicSlider.Value;
+            newSettings._SFXLevel = (float)_SFXSlider.Value;
+        }
+
+        // Save & Load the new settings
+        GetNode<GameController>("/root/GameController").SaveSettings(newSettings);
+        GetNode<GameController>("/root/GameController").LoadSettings();
+    }
+
+    public void OnBackPressed()
+    {
+        this.Hide();
+        GetNode<Node2D>("/root/Main/Canvas/ColorRect/OptionsMenu").Show();
+    }
+}
+
+[Serializable]
+public class Settings
+{
+    public float _MusicLevel;
+    public float _SFXLevel;
 }
