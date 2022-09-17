85f3e8b87fb7268c95968b22227d94f45737c87d
EI.KMiller <KMiller.eilexinteractive@gmail.com>
2022-09-17 13:17:51 +1000

    Created Fireball weapon

---
+++ Scripts/Items/FireBallController.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public class FireBallController : Area2D
+{
+
+    [Export] private float _MovementSpeed = 10;
+    [Export] private float FireballDamage = 35;
+    [Export] private float DestroyTime = 3.0f;                      // How long before the object leaves the scene
+ 
+    private Timer _destroyTimer;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _destroyTimer = new Timer(DestroyTime, false, Destroy);
+        LookAt(GetNode<PlayerController>("/root/Main/Player").Position); 
+    }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Process(float delta)
+    {
+        
+        Position += new Vector2(_MovementSpeed, 0).Rotated(Rotation);
+
+        if(_destroyTimer != null)
+            _destroyTimer.UpdateTimer(delta);
+    }
+
+    public void OnFireballHit(Node body)
+    {
+        if(body is PlayerController)
+        {
+            var player = body as PlayerController;
+            player.GetOwningCharacter().TakeDamage(FireballDamage);
+        } else 
+        {
+            GD.Print("Hit Something");
+        }
+
+
+        QueueFree();
+    }
+
+    public void Destroy() => QueueFree();
+}
